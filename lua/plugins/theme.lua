8c1946721367955a2239faf27d660a1f6fc13367
Paloth <lth.paul@hotmail.fr>
2024-06-30 17:24:45 +0200

    refactor(theme): setup transparent backgroup and comment style

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,6 +5,12 @@ return {
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    require('catppuccin').setup {
+      transparent_background = true,
+      styles = {
+        comments = { 'italic' },
+      },
+    }
     vim.cmd.colorscheme 'catppuccin-macchiato'
 
     -- You can configure highlights by doing something like
