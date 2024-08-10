1f96462cee0516a30d315b239537d70d4b0d9712
Paloth <lth.paul@hotmail.fr>
2024-08-10 13:44:23 +0200

    switch to tokyon night

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,17 +1,8 @@
 return {
-  'catppuccin/nvim',
+  'folke/tokyonight.nvim',
   priority = 1000, -- make sure to load this before all the other start plugins
   init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    require('catppuccin').setup {
-      transparent_background = true,
-      styles = {
-        comments = { 'italic' },
-      },
-    }
-    vim.cmd.colorscheme 'catppuccin-macchiato'
+    vim.cmd.colorscheme 'tokyonight-night'
 
     -- You can configure highlights by doing something like
     vim.cmd.hi 'Comment gui=none'
