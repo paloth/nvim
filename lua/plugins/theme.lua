708e397b445d854edac442e613fec5584ce592b8
Paloth <lth.paul@hotmail.fr>
2024-12-10 18:57:48 +0100

    switch from tokyo night to one dark pro vivid

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,34 @@
 return {
-  'folke/tokyonight.nvim',
+  'olimorris/onedarkpro.nvim',
   priority = 1000, -- make sure to load this before all the other start plugins
   init = function()
-    vim.cmd.colorscheme 'tokyonight-night'
+    vim.cmd.colorscheme 'onedark_vivid'
 
     -- You can configure highlights by doing something like
     vim.cmd.hi 'Comment gui=none'
+  end,
+  config = function()
+    require('onedarkpro').setup {
+      styles = { -- For example, to apply bold and italic, use "bold,italic"
+        types = 'italic', -- Style that is applied to types
+        methods = 'NONE', -- Style that is applied to methods
+        numbers = 'NONE', -- Style that is applied to numbers
+        strings = 'NONE', -- Style that is applied to strings
+        comments = 'italic', -- Style that is applied to comments
+        keywords = 'bold,italic', -- Style that is applied to keywords
+        constants = 'bold', -- Style that is applied to constants
+        functions = 'bold,italic', -- Style that is applied to functions
+        operators = 'NONE', -- Style that is applied to operators
+        variables = 'NONE', -- Style that is applied to variables
+        parameters = 'NONE', -- Style that is applied to parameters
+        conditionals = 'italic', -- Style that is applied to conditionals
+        virtual_text = 'NONE', -- Style that is applied to virtual text
+      },
+      plugins = { -- Override which plugin highlight groups are loaded
+        telescope = false,
+        toggleterm = false,
+        treesitter = true,
+      },
+    }
   end,
 }
