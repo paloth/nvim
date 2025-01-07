afc7ad8bff289d75d9de2c63e4898598959fc5b4
Paul LOUIS THERESE <53221698+paloth@users.noreply.github.com>
2025-01-07 11:21:11 +0100

    nvim(plugin/lsp): add filetypes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -91,6 +91,9 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- clangd = {},
+      terraformls = {
+        filetypes = { 'terraform', 'terraform-vars' },
+      },
       gopls = {
         filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
         cmd = { 'gopls' },
