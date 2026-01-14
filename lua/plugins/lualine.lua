787ed211e044e0c422b2387fe83ebcaa9e949db6
Paul LOUIS THERESE <53221698+paloth@users.noreply.github.com>
2026-01-14 22:09:18 +0100

    enh(lualine): add file path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -42,7 +42,7 @@ return {
       },
       sections = {
         lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-        lualine_b = { 'filename', 'branch' },
+        lualine_b = { { 'filename', path = 1 }, 'branch' },
         lualine_c = { '%=' },
         lualine_x = {},
         lualine_y = { 'diff', 'diagnostics', 'filetype', 'progress' },
